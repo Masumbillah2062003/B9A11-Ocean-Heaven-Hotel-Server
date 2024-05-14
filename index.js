ee5d45507f613533436ab7bc3e5314f87233a81a
Masum Billah <masumbillah2062003@gmail.com>
2024-05-14 16:00:16 +0600

    added room route on the  serversite

--- index.js
+++ index.js
@@ -93,6 +93,20 @@ async function run() {
 
 
 
+app.put("/rooms/:id", async (req, res) => {
+      const id = req.params.id
+      const filter = { _id: new ObjectId(id) }
+      const updatedBooking = req.body
+      console.log(updatedBooking)
+      const updateDoc = {
+        $set: {
+          status: updatedBooking.status
+        },
+      };
+      const result = await roomCollection.updateOne(filter, updateDoc);
+      res.send(result)
+    })
+
 
 
     // app.get('/bookings', async (req, res) => {
